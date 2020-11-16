24c878d8559d2170c11e187f23eb75d4a860f1d8
Gaetan Jagorel <gaetan.jagorel@etu.univ-tours.fr>
2020-11-16 18:22:58 +0100

    Ajout IHM

--- TP3/app/src/main/java/com/example/tp3/MainActivity.java
+++ TP3/app/src/main/java/com/example/tp3/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.tp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+    private Button btnConnexion;
+    private Button btnDeconnexion;
+    private Button btnStop;
+
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnStart = findViewById(R.id.buttonStart);
+        this.btnConnexion = findViewById(R.id.buttonConnexion);
+        this.btnDeconnexion = findViewById(R.id.buttonDeconnexion);
+        this.btnStop = findViewById(R.id.buttonStop);
+
+        this.editText = findViewById(R.id.editText);
     }
 }
